42a1fcc86e6e3f50151cd71f4fffe1a3014668b9
RutujaV1129 <rutujav@packtpub.com>
2017-09-29 12:17:23 +0530

    Code Files
    Commit

---
+++ WSO2DevelpersGuide_Code/Chapter04/WSO2EI_Chapter4_Scripts.sql
@@ -0,0 +1,177 @@
+CREATE TABLE 'city' (
+  'ID' int(11) NOT NULL AUTO_INCREMENT,
+  'Name' char(35) NOT NULL DEFAULT '',
+  'CountryCode' char(3) NOT NULL DEFAULT '',
+  'District' char(20) NOT NULL DEFAULT '',
+  'Population' int(11) NOT NULL DEFAULT '0' ,
+  PRIMARY KEY ('ID')   
+)  AUTO_INCREMENT=1;
+
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Kabul', 'AFG', 'Kabol', '1780000');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Qandahar', 'AFG', 'Qandahar', '237500');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Herat', 'AFG', 'Herat', '186800');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Mazar-e-Sharif', 'AFG', 'Balkh', '127800');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Amsterdam', 'NLD', 'Noord-Holland', '731200');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Rotterdam', 'NLD', 'Zuid-Holland', '593321');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Haag', 'NLD', 'Zuid-Holland', '440900');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Utrecht', 'NLD', 'Utrecht', '234323');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Eindhoven', 'NLD', 'Noord-Brabant', '201843');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Tilburg', 'NLD', 'Noord-Brabant', '193238');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Groningen', 'NLD', 'Groningen', '172701');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Breda', 'NLD', 'Noord-Brabant', '160398');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Apeldoorn', 'NLD', 'Gelderland', '153491');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Nijmegen', 'NLD', 'Gelderland', '152463');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Enschede', 'NLD', 'Overijssel', '149544');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Haarlem', 'NLD', 'Noord-Holland', '148772');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Almere', 'NLD', 'Flevoland', '142465');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Arnhem', 'NLD', 'Gelderland', '138020');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Zaanstad', 'NLD', 'Noord-Holland', '135621');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Amersfoort', 'NLD', 'Utrecht', '126270');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Maastricht', 'NLD', 'Limburg', '122087');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Dordrecht', 'NLD', 'Zuid-Holland', '119811');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Leiden', 'NLD', 'Zuid-Holland', '117196');
+INSERT INTO 'wso2ei'.'city'
+('Name',
+'CountryCode',
+'District',
+'Population')
+VALUES
+('Haarlemmermeer', 'NLD', 'Noord-Holland', '110722');
---
+++ WSO2DevelpersGuide_Code/README.txt
@@ -0,0 +1,3 @@
+This book will guide you through the installation of all the tools that you require to follow the examples. 
+
+Chapter 13 does not have any code.
